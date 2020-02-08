413ebc8fa65fd8da3ed78cd1385342bbd5424edd
Nathaniel Pather <nathaniel_pather@hotmail.com>
2020-02-08 21:30:10 +1000

    Create comments model.

---
+++ app/Comments.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comments extends Model
+{
+    protected $fillable  = [
+        'comment', 'post_id', 'user_id'
+    ];
+
+    public function post() {
+        return $this->belongsTo('App\Posts');
+    }
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+}
